3250d33eb6ca10dfd0b835f70cfcc59fc5f02964
Brian Travis <bztravis88@gmail.com>
2024-03-12 23:46:05 -0400

    Add join hero structure

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,28 @@ export default function LandingPage() {
         </div>
       </nav>
       <main>
-        <div className='flex flex-row justify-center items-center h-96'>
+        <div className='flex flex-col justify-center items-center gap-24 h-[640px] text-lg'>
           <h1 className='max-w-[960px] text-center text-7xl font-semibold tracking-tight'>
             Understand, engage, and connect your audience.
           </h1>
+          <div className='flex flex-col gap-8 items-center'>
+            <div className='flex flex-row gap-2 p-2 w-[500px] h-20 border rounded bg-gray-200'>
+              <input
+                type='text'
+                placeholder='Enter your event code'
+                className='p-2 grow'
+              />
+              <button className='px-8 py-2 rounded bg-tangerine500 font-medium'>
+                Join
+              </button>
+            </div>
+            <span className='flex gap-4'>
+              <p className='inline'>Organizing an event?</p>
+              <Link href='/signin' className='underline'>
+                Sign In
+              </Link>
+            </span>
+          </div>
         </div>
       </main>
     </div>
