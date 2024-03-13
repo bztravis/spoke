430c6966abc1403642b8a209a52bb289fca4dc1e
Brian Travis <bztravis88@gmail.com>
2024-03-12 23:05:27 -0400

    Add branding metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'Spoke: Understand, engage, and connect your audience',
+};
+
 export default function LandingPage() {
   return <main>Welcome to Spoke</main>;
 }
